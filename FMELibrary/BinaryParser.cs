98baae6d187a83794b0114f00bbdd1aa0662d6ed
Nyong Grandong <nyongrand@gmail.com>
2023-02-13 22:25:01 +0700

    add competitin parser

---
+++ FMELibrary/BinaryParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace FMELibrary
+{
+    public static class BinaryParser
+    {
+        public static string ReadString(this BinaryReader reader, int length)
+        {
+            return Encoding.UTF8.GetString(reader.ReadBytes(length));
+        }
+
+        public static byte[] GetBytes(this string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            return BitConverter.GetBytes(bytes.Length).Concat(bytes).ToArray();
+        }
+
+        //public static Color ReadColor(this BinaryReader reader)
+        //{
+        //    int color = reader.ReadInt16();
+        //    return Color.FromArgb(color / (1 << 10), color % (1 << 10) / (1 << 5), color % (1 << 5));
+        //}
+
+        //public static byte[] GetBytes(this Color color)
+        //{
+        //    short s = (color.R << 10) | (color.G << 5) | color.B;
+
+        //    var bytes = Encoding.UTF8.GetBytes(text);
+        //    return BitConverter.GetBytes(bytes.Length).Concat(bytes).ToArray();
+        //}
+    }
+}
--- FMELibrary/CompetitionParser.cs
+++ FMELibrary/CompetitionParser.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Reflection.Metadata.BlobBuilder;
-
-namespace FMELibrary
+﻿namespace FMELibrary
 {
     public class CompetitionParser
     {
@@ -22,38 +14,27 @@ namespace FMELibrary
         /// </summary>
         public int Count { get; set; }
 
-        public List<Competition> Items
-        {
-            get
-            {
-                if (items == null)
-                    throw new InvalidOperationException("Parse() is not called");
-
-                return items;
-            }
-
-            set => items = value;
-        }
-
-        private List<Competition>? items = null;
+        /// <summary>
+        /// List of all items
+        /// </summary>
+        public List<Competition> Items { get; set; }
 
         public CompetitionParser(string path)
         {
-            FilePath = path;
-
-            using var file = File.OpenRead(FilePath);
+            using var file = File.OpenRead(path);
             using var reader = new BinaryReader(file);
 
+            FilePath = path;
             Header = reader.ReadBytes(8);
-            Count = reader.ReadInt32();
+            Count = reader.ReadInt16();
             Items = new List<Competition>();
 
             while (file.Position < file.Length)
             {
                 try
                 {
-                    var item = Read(reader);
-                    item.Id = Items.Count;
+                    var item = new Competition(reader);
+                    var bytes = Convert.ToHexString(item.ToBytes());
                     Items.Add(item);
                 }
                 catch
@@ -64,58 +45,6 @@ namespace FMELibrary
 
                 if (Items.Count == Count) break;
             }
-        }
-
-        private static Competition Read(BinaryReader reader)
-        {
-            var item = new Competition
-            {
-                Uid = reader.ReadInt32(),
-                FullName = ReadString(reader)
-            };
-
-            reader.ReadByte();
-            item.ShortName = ReadString(reader);
-
-            reader.ReadByte();
-            item.CodeName = ReadString(reader);
-
-            var len = GetOthersLength(item.Uid);
-            item.Others1 = reader.ReadBytes(len);
-
-            return item;
-        }
-
-        private static string ReadString(BinaryReader reader)
-        {
-            var length = reader.ReadInt32();
-            return Encoding.UTF8.GetString(reader.ReadBytes(length));
-        }
-
-        private static int GetOthersLength(int uid)
-        {
-            return uid switch
-            {
-                40 => 263, // Major League Soccer
-                90 => 503, // European International League
-                91 => 167, // European International League Division A
-                92 => 167, // European International League Division B
-                93 => 167, // European International League Division C
-                94 => 95,  // European International League Division D
-
-                100104 => 359, // World Cup African Qualifying Section
-                100105 => 583, // World Cup European Qualifying Section
-
-                102415 => 791, // Copa Libertadores
-                127286 => 415, // Asian Champions League
-                127299 => 471, // African Champions League
-
-                136408 => 239, // Korean FA Cup
-                145509 => 47,  // North American Gold Cup
-                157149 => 55,  // North American U20 Championship
-                219740 => 191, // North American League
-                _ => 39,
-            };
         }
     }
 }
