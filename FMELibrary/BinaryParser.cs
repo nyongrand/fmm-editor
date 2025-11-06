30a50cde9214432ab2b8e28237243884a38d2a39
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-06 22:08:44 +0700

    Refactor BinaryParser string and write methods
    Renamed ReadStringFF to ReadStringEx and removed the 0xFF terminator logic. Consolidated multiple WriteEx overloads into a single method using type checks to handle various types, including string and Color. Removed WriteStringFF and WriteEx(Color) methods for improved maintainability.

--- FMELibrary/BinaryParser.cs
+++ FMELibrary/BinaryParser.cs
@@ -10,12 +10,10 @@ namespace FMELibrary
             return Encoding.UTF8.GetString(reader.ReadBytes(length));
         }
 
-        public static string ReadStringFF(this BinaryReader reader)
+        public static string ReadStringEx(this BinaryReader reader)
         {
             int len = reader.ReadInt32();
-            string s = reader.ReadString(len);
-            reader.ReadByte(); // Terminator, 0xFF
-            return s;
+            return reader.ReadString(len);
         }
 
         public static Color ReadColor(this BinaryReader reader)
@@ -28,26 +26,41 @@ namespace FMELibrary
             return Color.FromArgb(red << 3, green << 3, blue << 3);
         }
 
-        public static void WriteEx(this BinaryWriter writer, string text)
+        public static void WriteEx(this BinaryWriter writer, object? value)
         {
-            var bytes = Encoding.UTF8.GetBytes(text);
-            writer.Write(bytes.Length);
-            writer.Write(bytes);
-        }
-
-        public static void WriteEx(this BinaryWriter writer, Color color)
-        {
-            int red = color.R >> 3;
-            int green = color.G >> 3;
-            int blue = color.B >> 3;
-            short s = (short)((red << 10) | (green << 5) | blue);
-            writer.Write(s);
-        }
-
-        public static void WriteStringFF(this BinaryWriter writer, string text, byte terminator = 0xFF)
-        {
-            writer.WriteEx(text);
-            writer.Write(terminator);
+            switch (value)
+            {
+                case byte b:
+                    writer.Write(b);
+                    break;
+                case short s:
+                    writer.Write(s);
+                    break;
+                case int i:
+                    writer.Write(i);
+                    break;
+                case float f:
+                    writer.Write(f);
+                    break;
+                case byte[] b:
+                    writer.Write(b);
+                    break;
+                case string s:
+                    var bytes = Encoding.UTF8.GetBytes(s);
+                    writer.Write(bytes.Length);
+                    writer.Write(bytes);
+                    break;
+                case Color c:
+                    int red = c.R >> 3;
+                    int green = c.G >> 3;
+                    int blue = c.B >> 3;
+                    writer.Write((short)((red << 10) | (green << 5) | blue));
+                    break;
+                case null:
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unsupported type: {value?.GetType().FullName}");
+            }
         }
     }
 }
