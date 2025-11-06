ea1e4ab4e3fbf23560800ac4dbe987d07b067d87
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-06 22:09:49 +0700

    Refactor Nation class structure and serialization
    Updated the Nation class to clarify and reorganize its fields, introducing new properties and making several fields nullable. Improved the BinaryReader and BinaryWriter logic for more accurate serialization and deserialization, including conditional reading/writing based on IsActive and HasCoefficient2. Replaced custom string reading with ReadStringEx and added default initializations for array properties.

--- FMELibrary/Nation.cs
+++ FMELibrary/Nation.cs
@@ -15,91 +15,100 @@
         public short CityId { get; set; }
         public short StadiumId { get; set; }
 
-        public byte[] Unknown1 { get; set; }
+        public int Unknown1 { get; set; }
+        public short Unknown2 { get; set; }
+        public byte Unknown3 { get; set; }
 
         public (short, byte)[] Languages { get; set; }
 
-        public byte Unknown2 { get; set; }
-        public short Color1 { get; set; }
-        public int Unknown3 { get; set; }
-        public short Color2 { get; set; }
+        public byte IsActive { get; set; }
+        public short? Color1 { get; set; }
+        public int? Unknown5 { get; set; }
+        public short? Color2 { get; set; }
 
-        public byte Unknown4 { get; set; }
-        public short Unknown5 { get; set; }
-        public byte Unknown6 { get; set; }
+        public byte? Unknown6 { get; set; }
+        public short? Unknown7 { get; set; }
+        public byte? Unknown8 { get; set; }
 
-        public byte IsRanked { get; set; }
-        public short Ranking { get; set; }
-        public short Points { get; set; }
+        public byte? IsRanked { get; set; }
+        public short? Ranking { get; set; }
+        public short? Points { get; set; }
 
-        public short Unknown7 { get; set; }
-        public float[] Coefficients1 { get; set; }
+        public short? Unknown9 { get; set; }
+        public float[] Coefficients1 { get; set; } = [];
 
         //public (string, short, byte)[] ExtraNames { get; set; }
-        public byte[] Unknown12 { get; set; }
-        public byte UnknownType { get; set; }
-        public byte[] Unknown14 { get; set; }
-        public float[] Coefficients2 { get; set; }
+        public byte[] Unknown10 { get; set; } = [];
+        public byte HasCoefficient2 { get; set; }
+        public byte[] Unknown11 { get; set; } = [];
+        public byte? Unknown12 { get; set; }
+        public short? Unknown13 { get; set; }
+        public float[] Coefficients2 { get; set; } = [];
+        public byte[] Unknown14 { get; set; } = [];
 
         public Nation(BinaryReader reader)
         {
             Uid = reader.ReadInt32();
             Id = reader.ReadInt16();
 
-            Name = reader.ReadString(reader.ReadInt32());
+            Name = reader.ReadStringEx();
             Terminator1 = reader.ReadByte();
-            Nationality = reader.ReadString(reader.ReadInt32());
+            Nationality = reader.ReadStringEx();
             Terminator2 = reader.ReadByte();
-            CodeName = reader.ReadString(reader.ReadInt32());
+            CodeName = reader.ReadStringEx();
 
             ContinentId = reader.ReadInt16();
             CityId = reader.ReadInt16();
             StadiumId = reader.ReadInt16();
 
-            Unknown1 = reader.ReadBytes(7);
+            Unknown1 = reader.ReadInt32();
+            Unknown2 = reader.ReadInt16();
+            Unknown3 = reader.ReadByte();
 
             Languages = new (short, byte)[reader.ReadByte()];
             for (int i = 0; i < Languages.Length; i++)
                 Languages[i] = (reader.ReadInt16(), reader.ReadByte());
 
-            Unknown2 = reader.ReadByte();
-            Color1 = reader.ReadInt16();
-            Unknown3 = reader.ReadInt32();
-            Color2 = reader.ReadInt16();
-
-            Unknown4 = reader.ReadByte();
-            Unknown5 = reader.ReadInt16();
-            Unknown6 = reader.ReadByte();
-
-            IsRanked = reader.ReadByte();
-            Ranking = reader.ReadInt16();
-            Points = reader.ReadInt16();
-
-            Unknown7 = reader.ReadInt16();
-
-            Coefficients1 = new float[reader.ReadByte()];
-            for (int i = 0; i < Coefficients1.Length; i++)
-                Coefficients1[i] = reader.ReadSingle();
-
-            //ExtraNames = new (string, short, byte)[reader.ReadByte()];
-            //for (int i = 0; i < ExtraNames.Length; i++)
-            //    ExtraNames[i] = (reader.ReadString(reader.ReadInt32()), reader.ReadInt16(), reader.ReadByte());
-
-            Unknown12 = reader.ReadBytes(11);
-            UnknownType = reader.ReadByte();
-            if (UnknownType == 0)
-                Unknown14 = [];
-            else
+            IsActive = reader.ReadByte();
+            if (IsActive == 1)
             {
-                Unknown14 = reader.ReadBytes(16);
-                reader.ReadByte();
-                reader.ReadInt16();
+                Color1 = reader.ReadInt16();
+                Unknown5 = reader.ReadInt32();
+                Color2 = reader.ReadInt16();
+
+                Unknown6 = reader.ReadByte();
+                Unknown7 = reader.ReadInt16();
+                Unknown8 = reader.ReadByte();
+
+                IsRanked = reader.ReadByte();
+                Ranking = reader.ReadInt16();
+                Points = reader.ReadInt16();
+
+                Unknown9 = reader.ReadInt16();
+
+                Coefficients1 = new float[reader.ReadByte()];
+                for (int i = 0; i < Coefficients1.Length; i++)
+                    Coefficients1[i] = reader.ReadSingle();
+
+                //ExtraNames = new (string, short, byte)[reader.ReadByte()];
+                //for (int i = 0; i < ExtraNames.Length; i++)
+                //    ExtraNames[i] = (reader.ReadString(reader.ReadInt32()), reader.ReadInt16(), reader.ReadByte());
+
+                Unknown10 = reader.ReadBytes(11);
+            }
+
+            HasCoefficient2 = reader.ReadByte();
+            if (HasCoefficient2 == 1)
+            {
+                Unknown11 = reader.ReadBytes(16);
+                Unknown12 = reader.ReadByte();
+                Unknown13 = reader.ReadInt16();
 
                 Coefficients2 = new float[reader.ReadByte()];
                 for (int i = 0; i < Coefficients2.Length; i++)
                     Coefficients2[i] = reader.ReadSingle();
 
-                reader.ReadBytes(11);
+                Unknown14 = reader.ReadBytes(11);
             }
         }
 
@@ -113,36 +122,71 @@
 
         public void Write(BinaryWriter writer)
         {
-            //writer.Write(Uid);
-            //writer.Write(Id);
-            //writer.WriteEx(Name);
-            //writer.Write(Terminator1);
-            //writer.WriteEx(Nationality);
-            //writer.Write(Terminator2);
-            //writer.WriteEx(CodeName);
-            //writer.Write(ContinentId);
-            //writer.Write(CityId);
-            //writer.Write(StadiumId);
-            //writer.Write(Unknown1);
-            //writer.Write(Color1);
-            //writer.Write(Unknown3);
-            //writer.Write(Color2);
-            //writer.Write(Unknown5);
-            //writer.Write(Unknown6);
-            //writer.Write(Unknown7);
-            //writer.Write(Unknown7);
-            //writer.Write(Unknown9);
-            //writer.Write(Unknown10);
-            //writer.Write(IsRanked);
-            //writer.Write(Ranking);
-            //writer.Write(Points);
-            //writer.Write(Unknown11);
+            writer.WriteEx(Uid);
+            writer.WriteEx(Id);
 
-            //writer.Write((byte)Coefficients.Length);
-            //for (int i = 0; i < Coefficients.Length; i++)
-            //{
-            //    writer.Write(Coefficients[i]);
-            //}
+            writer.WriteEx(Name);
+            writer.WriteEx(Terminator1);
+            writer.WriteEx(Nationality);
+            writer.WriteEx(Terminator2);
+            writer.WriteEx(CodeName);
+
+            writer.WriteEx(ContinentId);
+            writer.WriteEx(CityId);
+            writer.WriteEx(StadiumId);
+
+            writer.WriteEx(Unknown1);
+            writer.WriteEx(Unknown2);
+            writer.WriteEx(Unknown3);
+
+            writer.WriteEx((byte)Languages.Length);
+            for (int i = 0; i < Languages.Length; i++)
+            {
+                writer.WriteEx(Languages[i].Item1);
+                writer.WriteEx(Languages[i].Item2);
+            }
+
+            writer.WriteEx(IsActive);
+
+            if (IsActive == 1)
+            {
+                writer.WriteEx(Color1);
+                writer.WriteEx(Unknown5);
+                writer.WriteEx(Color2);
+
+                writer.WriteEx(Unknown6);
+                writer.WriteEx(Unknown7);
+                writer.WriteEx(Unknown8);
+
+                writer.WriteEx(IsRanked);
+                writer.WriteEx(Ranking);
+                writer.WriteEx(Points);
+
+                writer.WriteEx(Unknown9);
+                writer.WriteEx((byte)Coefficients1.Length);
+                for (int i = 0; i < Coefficients1.Length; i++)
+                {
+                    writer.WriteEx(Coefficients1[i]);
+                }
+
+                writer.WriteEx(Unknown10);
+            }
+
+            writer.WriteEx(HasCoefficient2);
+            if (HasCoefficient2 == 1)
+            {
+                writer.WriteEx(Unknown11);
+                writer.WriteEx(Unknown12);
+                writer.WriteEx(Unknown13);
+
+                writer.WriteEx((byte)Coefficients2.Length);
+                for (int i = 0; i < Coefficients2.Length; i++)
+                {
+                    writer.WriteEx(Coefficients2[i]);
+                }
+
+                writer.WriteEx(Unknown14);
+            }
 
             //writer.Write((byte)ExtraNames.Length);
             //for (int i = 0; i < ExtraNames.Length; i++)
@@ -151,15 +195,6 @@
             //    writer.Write(ExtraNames[i].Item2);
             //    writer.Write(ExtraNames[i].Item3);
             //}
-
-            //writer.Write((byte)Languages.Length);
-            //for (int i = 0; i < Languages.Length; i++)
-            //{
-            //    writer.Write(Languages[i].Item1);
-            //    writer.Write(Languages[i].Item2);
-            //}
-
-            writer.Write(Unknown12);
         }
     }
 }
