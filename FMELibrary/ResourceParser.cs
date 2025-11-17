a12826034e35cb05f0c561a7db063fb47290a1bc
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-17 10:02:19 +0700

    Add resource parsing and string resource classes
    Introduces ResourceParser for loading, saving, and serializing club-related data from binary files. Adds StringResource with classes for parsing and writing various resource types such as clubs, nations, continents, competitions, stadiums, awards, and others.

---
+++ FMELibrary/ResourceParser.cs
@@ -0,0 +1,214 @@
+﻿using static FMELibrary.StringResource;
+
+namespace FMELibrary
+{
+    /// <summary>
+    /// Parses and manages club data from binary files.
+    /// </summary>
+    public class ResourceParser
+    {
+        /// <summary>
+        /// File path
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// File header, 8 bytes
+        /// </summary>
+        public byte[] Header { get; set; }
+
+        /// <summary>
+        /// List of all items
+        /// </summary>
+        public List<ClubName> Clubs { get; set; }
+
+        public List<NationName> Nations { get; set; }
+
+        public List<ContinentName> Continents { get; set; }
+
+        public List<CompetitionName> Competitions { get; set; }
+
+        public List<StadiumName> Stadiums { get; set; }
+
+        public List<AwardName> Awards { get; set; }
+
+        public List<OtherName> Agreements { get; set; }
+
+        public List<OtherName> Rivalries { get; set; }
+
+        public List<OtherName> Regions { get; set; }
+
+        public byte[] UnknownData { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceParser"/> class.
+        /// </summary>
+        /// <param name="path">The file path of the source data.</param>
+        /// <param name="reader">The binary reader containing the club data.</param>
+        private ResourceParser(string path, BinaryReader reader)
+        {
+            FilePath = path;
+            Header = reader.ReadBytes(8);
+
+            Clubs = [];
+            Nations = [];
+            Continents = [];
+            Competitions = [];
+            Stadiums = [];
+            Awards = [];
+            Agreements = [];
+            Rivalries = [];
+            Regions = [];
+            UnknownData = [];
+        }
+
+        /// <summary>
+        /// Asynchronously loads club data from the specified file path.
+        /// </summary>
+        /// <param name="path">The file path to load club data from.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the loaded <see cref="ClubParser"/> instance.</returns>
+        public static async Task<ResourceParser> Load(string path)
+        {
+            using var fs = File.OpenRead(path);
+            using var ms = new MemoryStream();
+            fs.CopyTo(ms);
+            ms.Position = 0;
+
+            using var reader = new BinaryReader(ms);
+            var parser = new ResourceParser(path, reader);
+
+            await Task.Run(() =>
+            {
+                var clubCount = reader.ReadInt32();
+                while (clubCount > parser.Clubs.Count)
+                {
+                    var item = new ClubName(reader);
+                    parser.Clubs.Add(item);
+                }
+
+                var nationCount = reader.ReadInt32();
+                while (nationCount > parser.Nations.Count)
+                {
+                    var item = new NationName(reader);
+                    parser.Nations.Add(item);
+                }
+
+                var continentCount = reader.ReadInt32();
+                while (continentCount > parser.Continents.Count)
+                {
+                    var item = new ContinentName(reader);
+                    parser.Continents.Add(item);
+                }
+
+                var competitionCount = reader.ReadInt32();
+                while (competitionCount > parser.Competitions.Count)
+                {
+                    var item = new CompetitionName(reader);
+                    parser.Competitions.Add(item);
+                }
+
+                var stadiumCount = reader.ReadInt32();
+                while (stadiumCount > parser.Stadiums.Count)
+                {
+                    var item = new StadiumName(reader);
+                    parser.Stadiums.Add(item);
+                }
+
+                var awardsCount = reader.ReadInt32();
+                while (awardsCount > parser.Awards.Count)
+                {
+                    var item = new AwardName(reader);
+                    parser.Awards.Add(item);
+                }
+
+                var agreementsCount = reader.ReadInt32();
+                while (agreementsCount > parser.Agreements.Count)
+                {
+                    var item = new OtherName(reader);
+                    parser.Agreements.Add(item);
+                }
+
+                var citiesCount = reader.ReadInt32();
+                while (citiesCount > parser.Rivalries.Count)
+                {
+                    var item = new OtherName(reader);
+                    parser.Rivalries.Add(item);
+                }
+
+                var unknownsCount = reader.ReadInt32();
+                while (unknownsCount > parser.Regions.Count)
+                {
+                    var item = new OtherName(reader);
+                    parser.Regions.Add(item);
+                }
+
+                parser.UnknownData = reader.ReadBytes((int)(ms.Length - ms.Position));
+            });
+
+            return parser;
+        }
+
+        /// <summary>
+        /// Converts the club data to a byte array for serialization.
+        /// </summary>
+        /// <returns>A byte array representing the serialized club data.</returns>
+        public byte[] ToBytes()
+        {
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+
+            writer.Write(Header);
+
+            writer.Write(Clubs.Count);
+            foreach (var item in Clubs)
+                item.Write(writer);
+
+            writer.Write(Nations.Count);
+            foreach (var item in Nations)
+                item.Write(writer);
+
+            writer.Write(Continents.Count);
+            foreach (var item in Continents)
+                item.Write(writer);
+
+            writer.Write(Competitions.Count);
+            foreach (var item in Competitions)
+                item.Write(writer);
+
+            writer.Write(Stadiums.Count);
+            foreach (var item in Stadiums)
+                item.Write(writer);
+
+            writer.Write(Awards.Count);
+            foreach (var item in Awards)
+                item.Write(writer);
+
+            writer.Write(Agreements.Count);
+            foreach (var item in Agreements)
+                item.Write(writer);
+
+            writer.Write(Rivalries.Count);
+            foreach (var item in Rivalries)
+                item.Write(writer);
+
+            writer.Write(Regions.Count);
+            foreach (var item in Regions)
+                item.Write(writer);
+
+            writer.Write(UnknownData);
+
+            return stream.ToArray();
+        }
+
+        /// <summary>
+        /// Save data back to file path
+        /// </summary>
+        /// <param name="filepath">Optional file path. If null, saves to the original file path.</param>
+        /// <returns>A task that represents the asynchronous save operation.</returns>
+        public async Task Save(string? filepath = null)
+        {
+            var bytes = ToBytes();
+            await File.WriteAllBytesAsync(filepath ?? FilePath, bytes);
+        }
+    }
+}
---
+++ FMELibrary/StringResource.cs
@@ -0,0 +1,153 @@
+﻿namespace FMELibrary
+{
+    public class StringResource
+    {
+        public class ClubName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string FullName { get; set; } = reader.ReadStringEx();
+            public byte FullNameTerminator { get; set; } = reader.ReadByte();
+            public string ShortName { get; set; } = reader.ReadStringEx();
+            public byte ShortNameTerminator { get; set; } = reader.ReadByte();
+            public string SixLetterName { get; set; } = reader.ReadStringEx();
+            public string ThreeLetterName { get; set; } = reader.ReadStringEx();
+
+            public override string ToString() => FullName;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(FullName);
+                writer.WriteEx(FullNameTerminator);
+                writer.WriteEx(ShortName);
+                writer.WriteEx(ShortNameTerminator);
+                writer.WriteEx(SixLetterName);
+                writer.WriteEx(ThreeLetterName);
+            }
+        }
+
+        public class NationName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string Name { get; set; } = reader.ReadStringEx();
+            public byte NameTerminator { get; set; } = reader.ReadByte();
+            public string Demonym { get; set; } = reader.ReadStringEx();
+            public byte DemonymTerminator { get; set; } = reader.ReadByte();
+            public string Code { get; set; } = reader.ReadStringEx();
+            public byte Unknown { get; set; } = reader.ReadByte();
+
+            public override string ToString() => Name;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(Name);
+                writer.WriteEx(NameTerminator);
+                writer.WriteEx(Demonym);
+                writer.WriteEx(DemonymTerminator);
+                writer.WriteEx(Code);
+                writer.WriteEx(Unknown);
+            }
+        }
+
+        public class ContinentName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string Name { get; set; } = reader.ReadStringEx();
+            public byte NameTerminator { get; set; } = reader.ReadByte();
+            public string CodeName { get; set; } = reader.ReadStringEx();
+            public string Demonym { get; set; } = reader.ReadStringEx();
+            public byte DemonymTerminator { get; set; } = reader.ReadByte();
+
+            public override string ToString() => Name;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(Name);
+                writer.WriteEx(NameTerminator);
+                writer.WriteEx(CodeName);
+                writer.WriteEx(Demonym);
+                writer.WriteEx(DemonymTerminator);
+            }
+        }
+
+        public class CompetitionName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string FullName { get; set; } = reader.ReadStringEx();
+            public byte FullNameTerminator { get; set; } = reader.ReadByte();
+            public string ShortName { get; set; } = reader.ReadStringEx();
+            public byte ShortNameTerminator { get; set; } = reader.ReadByte();
+            public string CodeName { get; set; } = reader.ReadStringEx();
+
+            public override string ToString() => FullName;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(FullName);
+                writer.WriteEx(FullNameTerminator);
+                writer.WriteEx(ShortName);
+                writer.WriteEx(ShortNameTerminator);
+                writer.WriteEx(CodeName);
+            }
+        }
+
+        public class StadiumName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string FullName { get; set; } = reader.ReadStringEx();
+            public string ShortName { get; set; } = reader.ReadStringEx();
+            public byte Terminator { get; set; } = reader.ReadByte();
+            public byte Unknown { get; set; } = reader.ReadByte();
+
+            public override string ToString() => FullName;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(FullName);
+                writer.WriteEx(ShortName);
+                writer.WriteEx(Terminator);
+                writer.WriteEx(Unknown);
+            }
+        }
+
+        public class AwardName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string FullName { get; set; } = reader.ReadStringEx();
+            public byte FullNameTerminator { get; set; } = reader.ReadByte();
+            public string ShortName { get; set; } = reader.ReadStringEx();
+            public byte ShortNameTerminator { get; set; } = reader.ReadByte();
+
+            public override string ToString() => FullName;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(FullName);
+                writer.WriteEx(FullNameTerminator);
+                writer.WriteEx(ShortName);
+                writer.WriteEx(ShortNameTerminator);
+            }
+        }
+
+        public class OtherName(BinaryReader reader)
+        {
+            public int Uid { get; set; } = reader.ReadInt32();
+            public string Name { get; set; } = reader.ReadStringEx();
+            public byte Terminator { get; set; } = reader.ReadByte();
+
+            public override string ToString() => Name;
+
+            public void Write(BinaryWriter writer)
+            {
+                writer.WriteEx(Uid);
+                writer.WriteEx(Name);
+                writer.WriteEx(Terminator);
+            }
+        }
+    }
+}
