81bca0e9cfe8ece24eaec500530988a7f5babb6a
Ibnu Hasan A. <ibnu.haar@gmail.com>
2025-11-21 15:32:06 +0700

    Refactor People class properties and deserialization
    Renamed and retyped several properties in the People class for clarity, including replacing unknown fields with more descriptive names for national team stats. Updated deserialization logic to match new property types and structure, and improved console output formatting for Uid. Changed Unknown20 from a list of tuples to an array of longs to reflect underlying data format.

--- FMELibrary/People.cs
+++ FMELibrary/People.cs
@@ -31,17 +31,16 @@
 
         public byte Type { get; set; }
 
-        public short Unknown5 { get; set; }
+        public int UnknownDate { get; set; }
 
-        public short Unknown6 { get; set; }
+        public short NationalCaps { get; set; }
 
-        public short Unknown7 { get; set; }
+        public short NationalGoal { get; set; }
 
-        public short Unknown8 { get; set; }
+        public byte NationalU21Caps { get; set; }
+        public byte NationalU21Goals { get; set; }
 
-        public short Unknown9 { get; set; }
-
-        public int Unknown10 { get; set; }
+        public int? Unknown10 { get; set; }
 
         public int RecentCall { get; set; }
 
@@ -75,11 +74,16 @@
 
         public byte Unknown20Count { get; set; }
 
-        public List<(int, int)> Unknown20 { get; set; }
+        public long[] Unknown20 { get; set; }
 
 
         private string? _firstName;
         private string? _lastName;
+
+        private static readonly int[] _specialIds = [
+            103409, 103607, 124521, 142512, 155126, 155129, 350411, 352719, 353194, 353514, 353954, 356951, 357788, 357873, 359538,
+            410436, 413732, 414872, 416746, 537851, 561318, 675472, 714504, 719130, 830987, 864223, 929769, 947423, 947676, 956666,
+        ];
 
         public People(BinaryReader reader, List<Name> firstNames, List<Name> lastNames)
         {
@@ -87,19 +91,15 @@
             Id = reader.ReadInt32();
             Uid = reader.ReadInt32();
 
-            if (Uid == 15048541)
-            {
-                // Debug breakpoint placeholder
-            }
-
             FirstNameId = reader.ReadInt32();
             LastNameId = reader.ReadInt32();
             CommonNameId = reader.ReadInt32();
 
             _firstName = firstNames.Where(x => x.Id == FirstNameId).FirstOrDefault()?.Value;
             _lastName = lastNames.Where(x => x.Id == LastNameId).FirstOrDefault()?.Value;
-            Console.WriteLine($"{Uid} - {Uid:X8}: {_firstName} {_lastName}");
+            Console.WriteLine($"{Uid} - {Convert.ToHexString(BitConverter.GetBytes(Uid))}: {_firstName} {_lastName}");
 
+            // read 2 bytes day of year and 2 bytes year
             DateOfBirth = reader.ReadDate();
 
             NationId = reader.ReadInt16();
@@ -111,19 +111,22 @@
             Ethnicity = reader.ReadByte();
             Unknown3 = reader.ReadInt32();
             Type = reader.ReadByte();
-            Unknown5 = reader.ReadInt16();
-            Unknown6 = reader.ReadInt16();
-            Unknown7 = reader.ReadInt16();
-            Unknown8 = reader.ReadInt16();
-            Unknown9 = reader.ReadInt16();
-            if (Type == 1)
-                Unknown10 = reader.ReadInt32();
+            UnknownDate = reader.ReadInt32();
 
+            NationalCaps = reader.ReadInt16();
+            NationalGoal = reader.ReadInt16();
+            NationalU21Caps = reader.ReadByte();
+            NationalU21Goals = reader.ReadByte();
+
+            Unknown10 = reader.ReadInt32();
             RecentCall = reader.ReadInt32();
+
             Unknown12 = reader.ReadInt16();
             Unknown13 = reader.ReadInt32();
             Unknown14 = reader.ReadInt32();
-            Unknown15 = reader.ReadInt32();
+
+            if (Type == 1 && !_specialIds.Contains(Uid))
+                Unknown15 = reader.ReadInt32();
 
             Unknown15a = reader.ReadInt32();
             Unknown15b = reader.ReadInt32();
@@ -133,37 +136,36 @@
 
             Unknown16 = reader.ReadByte();
             Unknown17 = reader.ReadByte();
-            if (Type != 1) // Only exists for Type != 1
+            if (Unknown17 != 0) // Only exists for Type != 1
             {
                 Unknown18 = reader.ReadInt32();
-                if (Unknown17 != 0)
-                    Unknown19 = reader.ReadInt16();
+                Unknown19 = reader.ReadInt16();
+            }
+            else
+            {
+                if (Type != 1)
+                    Unknown18 = reader.ReadInt32();
             }
 
             MainLanguageCount = reader.ReadByte();
-            MainLanguages = new (short, byte)[MainLanguageCount];
+            MainLanguages = new (short Id, byte Proficiency)[MainLanguageCount];
             for (int i = 0; i < MainLanguageCount; i++)
             {
                 MainLanguages[i] = (reader.ReadInt16(), reader.ReadByte());
             }
 
             OtherLanguageCount = reader.ReadByte();
-            OtherLanguages = new (short, byte)[OtherLanguageCount];
+            OtherLanguages = new (short Id, byte Proficiency)[OtherLanguageCount];
             for (int i = 0; i < OtherLanguageCount; i++)
             {
                 OtherLanguages[i] = (reader.ReadInt16(), reader.ReadByte());
             }
 
             Unknown20Count = reader.ReadByte();
-            Unknown20 = new List<(int, int)>(Unknown20Count);
+            Unknown20 = new long[Unknown20Count];
             for (int i = 0; i < Unknown20Count; i++)
             {
-                Unknown20.Add((reader.ReadInt32(), reader.ReadInt32()));
-            }
-
-            if (Uid == 15048541)
-            {
-                // Debug breakpoint placeholder
+                Unknown20[i] = reader.ReadInt64();
             }
         }
 
