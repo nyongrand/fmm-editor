d96b12994b7c542533d7cbdfa222e2473a8cab70
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-23 20:56:25 +0700

    Add Ethnicity enum to FMELibrary
    Introduces an Ethnicity enumeration to represent various ethnic backgrounds within the FMELibrary namespace.

---
+++ FMELibrary/Ethnicity.cs
@@ -0,0 +1,18 @@
+﻿namespace FMELibrary
+{
+    public enum Ethnicity
+    {
+        NorthenEuropean = 0,
+        MediteranianHispanic = 1,
+        NorthAfricanMiddleEastern = 2,
+        AfricanCaribean = 3,
+        Asian = 4,
+        SouthEastAsian = 5,
+        PacificIslander = 6,
+        NativeAmerican = 7,
+        NativeAustralian = 8,
+        MixedRace = 9,
+        EastAsian = 10,
+        Unknown = 11
+    }
+}
