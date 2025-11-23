a2dc541941271b5e6b19d1c81704f2cecd220716
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-23 20:57:03 +0700

    Add SwitchNationality utility for changing player nation
    Introduces the SwitchNationality class with a static Run method that updates player nationalities in the PeopleParser. Players with no national caps and a specific nation in their other nationalities will have their main nation switched, and the previous nation added to their other nationalities.

---
+++ FMEConsole/SwitchNationality.cs
@@ -0,0 +1,31 @@
+﻿using FMELibrary;
+
+namespace FMEConsole
+{
+    public static class SwitchNationality
+    {
+        public static async Task<PeopleParser> Run(string databaseFile, short nationId)
+        {
+            var parser = await PeopleParser.Load(databaseFile);
+
+            foreach (var item in parser.Items)
+            {
+                if (item.Type == 1 && item.NationalCaps == 0 && item.NationId != nationId && item.OtherNationalities.Contains(nationId))
+                {
+                    // Switch current nation
+                    var oldNation = item.NationId;
+                    item.NationId = nationId;
+                    // Remove Indonesia from other nationalities
+                    item.OtherNationalities.Remove(nationId);
+                    // Add old nation to other nationalities if not already present
+                    if (!item.OtherNationalities.Contains(oldNation))
+                    {
+                        item.OtherNationalities.Insert(0, oldNation);
+                    }
+                }
+            }
+
+            return parser;
+        }
+    }
+}
