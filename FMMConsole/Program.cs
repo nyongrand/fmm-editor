714faec09d4a241f20b8557fa0ba10d2186adaee
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-25 05:50:17 +0700

    Refactor Program.cs to use player data and update queries
    Replaces the previous people.dat-based logic with new logic using players.dat and related parsers. Updates the main query to join people, player, club, nation, and name data, and groups results by a player property. The VerifyDatFileAsync method now loads and compares players.dat, and all previous test and data manipulation code is commented out for clarity.

--- FMMConsole/Program.cs
+++ FMMConsole/Program.cs
@@ -23,118 +23,149 @@ using FMMLibrary;
 //await SwitchNationClubsWithTopContinentClubsAsync(
 //    [38, 39, 1385, 1386, 1411, 1412, 1413, 1414, 1415], (int)Confederation.UEFA);
 
-await VerifyDatFileAsync();
+//var file = "../../../db/db_archive_2603/player.dat";
+//var parser = await Scripts.SwitchNationality(file, 70);
+
+// Save changes back to file
+//await parser.Save();
+
+//var cparser = await ClubParser.Load("../../../db/db_archive_2603/club.dat");
+//var rangers = cparser.Items.Where(x => x.FullName.Contains("Rangers"));
+
+//var nparser = await NationParser.Load("../../../db/db_archive_2603/nation.dat");
+//var fparser = await NameParser.Load("../../../db/db_archive_2603/first_names.dat");
+//var lparser = await NameParser.Load("../../../db/db_archive_2603/second_names.dat");
+
+//var nf = lparser.Items.Where(x => x.NationUid == 113).ToList();
+//fparser.Add(new Name(0, 113, 1, 5, "Ernando"));
+//lparser.Add(new Name(0, 113, 1, 225, "Sutaryadi"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Nadeo"));
+//lparser.Add(new Name(0, 113, 1, 225, "Argawinata"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Ramadhan"));
+//lparser.Add(new Name(0, 113, 1, 225, "Sananta"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Cahya"));
+//lparser.Add(new Name(0, 113, 1, 225, "Supriadi"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Yance"));
+//lparser.Add(new Name(0, 113, 1, 225, "Sayuri"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Pratama"));
+//lparser.Add(new Name(0, 113, 1, 225, "Arhan"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Muhammad"));
+//lparser.Add(new Name(0, 113, 1, 225, "Ferarri"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Achmad"));
+//lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Arkhan"));
+//lparser.Add(new Name(0, 113, 1, 225, "Fikri"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Hokky"));
+//lparser.Add(new Name(0, 113, 1, 225, "Caraka"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Yakob"));
+//lparser.Add(new Name(0, 113, 1, 225, "Kaka"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Kakang"));
+//lparser.Add(new Name(0, 113, 1, 225, "Rudianto"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Ricky"));
+//lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Fajar"));
+//lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Bambang"));
+//lparser.Add(new Name(0, 113, 1, 225, "Darwis"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Kurnia"));
+//lparser.Add(new Name(0, 113, 1, 225, "Putra"));
+
+//fparser.Add(new Name(0, 113, 1, 5, "Septian"));
+//lparser.Add(new Name(0, 113, 1, 225, "Hasan"));
+
+//await fparser.Save();
+//await lparser.Save();
+
+//var query = from p in parser.Items
+//            join n in nparser.Items on p.NationId equals n.Id
+//            join f in fparser.Items on p.FirstNameId equals f.Id
+//            join l in lparser.Items on p.LastNameId equals l.Id
+//            select new
+//            {
+//                FirstName = f.Value,
+//                LastName = l.Value,
+//                Nation = n.Name,
+//                People = p,
+//            };
+
+//var peoples = query.ToArray();
+
+//var indonesian = peoples
+//    .GroupBy(x => x.People.Unknown2)
+//    .OrderBy(x => x.Key)
+//    //.Where(x => x.People.Unknown2 == 805)
+//    //.Where(x => x.People.Type == 1)
+//    //.Where(x => x.People.NationalCaps == 0)
+//    //.Where(x => x.People.NationId != 58)
+//    //.Where(x => x.People.NationId == 58 || x.People.OtherNationalities.Contains(58))
+//    //.GroupBy(x => x.Nation)
+//    //.Where(x => x.People.MainLanguages.Select(x => x.Id).ToList().Contains(76) || x.People.OtherLanguages.Select(x => x.Id).ToList().Contains(76))
+//    .ToList();
+
+//var g4 = peoples.GroupBy(x => x.Ethnicity).ToList();
+//var g5 = peoples.GroupBy(x => x.People.UnknownDate).ToList();
+
+//var str = "../../../db/db_archive_2603/eng.lng";
+//var strParser = await ResourceParser.Load(str);
+//var cities = strParser.Regions.OrderBy(x => x.Name).ToList();
+
+//var j = from c in parser.Items
+//        join s in strParser.Regions on c.Id equals s.Uid
+//        select new 
+//        {
+//            City = c, 
+//            Str = s 
+//        };
+
+var peopleParser = await PeopleParser.Load("../../../db/db_archive_2603/people.dat");
+var playerParser = await PlayerParser.Load("../../../db/db_archive_2603/players.dat");
+var clubParser = await ClubParser.Load("../../../db/db_archive_2603/club.dat");
+var nationParser = await NationParser.Load("../../../db/db_archive_2603/nation.dat");
+var fnameParser = await NameParser.Load("../../../db/db_archive_2603/first_names.dat");
+var lnameParser = await NameParser.Load("../../../db/db_archive_2603/second_names.dat");
+
+var query = from people in peopleParser.Items
+            join club in clubParser.Items on people.ClubId equals club.Id
+            join player in playerParser.Items on people.PlayerId equals player.Id
+            join nation in nationParser.Items on people.NationId equals nation.Id
+            join fname in fnameParser.Items on people.FirstNameId equals fname.Id
+            join lname in lnameParser.Items on people.LastNameId equals lname.Id
+            select new
+            {
+                FirstName = fname.Value,
+                LastName = lname.Value,
+                People = people,
+                Player = player,
+                Club = club,
+                Nation = nation.Name,
+            };
+
+var players = query.ToList();
+var grouped = players.GroupBy(x => x.Player.Unknown1).ToList();
+
+Console.ReadLine();
+
+//await VerifyDatFileAsync();
 
 static async Task VerifyDatFileAsync()
 {
-    var file = "../../../db/db_archive_2603/people.dat";
-    var parser = await Scripts.SwitchNationality(file, 70);
-
-    // Save changes back to file
-    //await parser.Save();
-
-    //var cparser = await ClubParser.Load("../../../db/db_archive_2603/club.dat");
-    //var rangers = cparser.Items.Where(x => x.FullName.Contains("Rangers"));
-
-    var nparser = await NationParser.Load("../../../db/db_archive_2603/nation.dat");
-    var fparser = await NameParser.Load("../../../db/db_archive_2603/first_names.dat");
-    var lparser = await NameParser.Load("../../../db/db_archive_2603/second_names.dat");
-
-    var nf = lparser.Items.Where(x => x.NationUid == 113).ToList();
-    fparser.Add(new Name(0, 113, 1, 5, "Ernando"));
-    lparser.Add(new Name(0, 113, 1, 225, "Sutaryadi"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Nadeo"));
-    lparser.Add(new Name(0, 113, 1, 225, "Argawinata"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Ramadhan"));
-    lparser.Add(new Name(0, 113, 1, 225, "Sananta"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Cahya"));
-    lparser.Add(new Name(0, 113, 1, 225, "Supriadi"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Yance"));
-    lparser.Add(new Name(0, 113, 1, 225, "Sayuri"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Pratama"));
-    lparser.Add(new Name(0, 113, 1, 225, "Arhan"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Muhammad"));
-    lparser.Add(new Name(0, 113, 1, 225, "Ferarri"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Achmad"));
-    lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Arkhan"));
-    lparser.Add(new Name(0, 113, 1, 225, "Fikri"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Hokky"));
-    lparser.Add(new Name(0, 113, 1, 225, "Caraka"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Yakob"));
-    lparser.Add(new Name(0, 113, 1, 225, "Kaka"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Kakang"));
-    lparser.Add(new Name(0, 113, 1, 225, "Rudianto"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Ricky"));
-    lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Fajar"));
-    lparser.Add(new Name(0, 113, 1, 225, "Maulana"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Bambang"));
-    lparser.Add(new Name(0, 113, 1, 225, "Darwis"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Kurnia"));
-    lparser.Add(new Name(0, 113, 1, 225, "Putra"));
-
-    fparser.Add(new Name(0, 113, 1, 5, "Septian"));
-    lparser.Add(new Name(0, 113, 1, 225, "Hasan"));
-
-    //await fparser.Save();
-    //await lparser.Save();
-
-    //var query = from p in parser.Items
-    //            join n in nparser.Items on p.NationId equals n.Id
-    //            join f in fparser.Items on p.FirstNameId equals f.Id
-    //            join l in lparser.Items on p.LastNameId equals l.Id
-    //            select new
-    //            {
-    //                FirstName = f.Value,
-    //                LastName = l.Value,
-    //                Nation = n.Name,
-    //                People = p,
-    //            };
-
-    //var peoples = query.ToArray();
-
-    //var indonesian = peoples
-    //    .GroupBy(x => x.People.Unknown2)
-    //    .OrderBy(x => x.Key)
-    //    //.Where(x => x.People.Unknown2 == 805)
-    //    //.Where(x => x.People.Type == 1)
-    //    //.Where(x => x.People.NationalCaps == 0)
-    //    //.Where(x => x.People.NationId != 58)
-    //    //.Where(x => x.People.NationId == 58 || x.People.OtherNationalities.Contains(58))
-    //    //.GroupBy(x => x.Nation)
-    //    //.Where(x => x.People.MainLanguages.Select(x => x.Id).ToList().Contains(76) || x.People.OtherLanguages.Select(x => x.Id).ToList().Contains(76))
-    //    .ToList();
-
-    //var g4 = peoples.GroupBy(x => x.Ethnicity).ToList();
-    //var g5 = peoples.GroupBy(x => x.People.UnknownDate).ToList();
-
-    //var str = "../../../db/db_archive_2603/eng.lng";
-    //var strParser = await ResourceParser.Load(str);
-    //var cities = strParser.Regions.OrderBy(x => x.Name).ToList();
-
-    //var j = from c in parser.Items
-    //        join s in strParser.Regions on c.Id equals s.Uid
-    //        select new 
-    //        {
-    //            City = c, 
-    //            Str = s 
-    //        };
+    var file = "../../../db/db_archive_2603/players.dat";
+    var parser = await PlayerParser.Load("../../../db/db_archive_2603/players.dat");
 
     var bytes1 = parser.ToBytes();
     var bytes2 = await File.ReadAllBytesAsync(file);
@@ -146,6 +177,7 @@ static async Task VerifyDatFileAsync()
         if (byte1 != byte2)
         {
             // difference handling placeholder
+            throw new Exception($"Byte mismatch at position {i}: {byte1} != {byte2}");
         }
     }
 
