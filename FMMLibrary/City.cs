407bc43d0ebcc8059900a5b2c536c17ec86a0f90
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-11-26 05:44:50 +0700

    Refactor City class with explicit properties
    Replaced ambiguous Unknown fields in the City class with explicit properties: Latitude, Longitude, Attraction, RegionId, and Unknown1. Updated the constructor and serialization logic to match the new structure for improved code clarity and maintainability.

--- FMMLibrary/City.cs
+++ FMMLibrary/City.cs
@@ -3,28 +3,24 @@
     public class City
     {
         public short Id { get; set; }
-
         public int Uid { get; set; }
-
         public short NationId { get; set; }
-
-        public byte[] Unknown2 { get; set; }
-        public byte Unknown3 { get; set; }
-        public short Unknown4 { get; set; }
-        public byte Unknown5 { get; set; }
-        public byte Unknown6 { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public byte Attraction { get; set; }
+        public short RegionId { get; set; }
+        public byte Unknown1 { get; set; }
 
         public City(BinaryReader reader)
         {
             Id = reader.ReadInt16();
             Uid = reader.ReadInt32();
-
             NationId = reader.ReadInt16();
-            Unknown2 = reader.ReadBytes(7);
-            Unknown3 = reader.ReadByte();
-            Unknown4 = reader.ReadInt16();
-            Unknown5 = reader.ReadByte();
-            Unknown6 = reader.ReadByte();
+            Latitude = reader.ReadSingle();
+            Longitude = reader.ReadSingle();
+            Attraction = reader.ReadByte();
+            RegionId = reader.ReadInt16();
+            Unknown1 = reader.ReadByte();
         }
 
         /// <summary>
@@ -48,11 +44,16 @@
             writer.WriteEx(Id);
             writer.WriteEx(Uid);
             writer.WriteEx(NationId);
-            writer.WriteEx(Unknown2);
-            writer.WriteEx(Unknown3);
-            writer.WriteEx(Unknown4);
-            writer.WriteEx(Unknown5);
-            writer.WriteEx(Unknown6);
+            writer.WriteEx(Latitude);
+            writer.WriteEx(Longitude);
+            writer.WriteEx(Attraction);
+            writer.WriteEx(RegionId);
+            writer.WriteEx(Unknown1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Uid}";
         }
     }
 }
