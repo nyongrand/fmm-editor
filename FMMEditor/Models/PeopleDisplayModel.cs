8d1a3fde630be1efc95005a9c44bb15d9ad11d08
Ibnu Hasan Arosyidin <ibnu.haar@gmail.com>
2025-12-05 20:50:39 +0700

    Move PeopleDisplayModel to Models namespace
    Extracted PeopleDisplayModel from ViewModels/PeopleViewModel.cs to a new file in Models/PeopleDisplayModel.cs for better separation of concerns and code organization.

---
+++ FMMEditor/Models/PeopleDisplayModel.cs
@@ -0,0 +1,30 @@
+﻿using FMMEditor.Converters;
+using FMMLibrary;
+
+namespace FMMEditor.Models
+{
+    /// <summary>
+    /// Display model for People with resolved names
+    /// </summary>
+    public class PeopleDisplayModel(People person)
+    {
+        public People Person { get; set; } = person;
+        public int Uid => Person.Uid;
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string CommonName { get; set; } = "";
+        public string FullName => !string.IsNullOrEmpty(CommonName) ? CommonName : $"{FirstName} {LastName}".Trim();
+        public string NationName { get; set; } = "";
+        public string ClubName { get; set; } = "";
+        public string DateOfBirth => DateOfBirthConverter.ToDateString(Person.DateOfBirth);
+        public string PersonType => Person.Type switch { 0 => "Player", 1 => "Staff", _ => "Other" };
+        public short NationalCaps => Person.NationalCaps;
+        public short NationalGoals => Person.NationalGoals;
+        public byte Adaptability => Person.Adaptability;
+        public byte Ambition => Person.Ambition;
+        public byte Loyalty => Person.Loyality;
+        public byte Pressure => Person.Pressure;
+        public byte Professionalism => Person.Professionalism;
+        public byte Temperament => Person.Temperament;
+    }
+}
--- FMMEditor/ViewModels/PeopleViewModel.cs
+++ FMMEditor/ViewModels/PeopleViewModel.cs
@@ -1,5 +1,6 @@
 using FMMEditor.Collections;
 using FMMEditor.Converters;
+using FMMEditor.Models;
 using FMMLibrary;
 using MaterialDesignThemes.Wpf;
 using MvvmDialogs;
@@ -18,36 +19,6 @@ using System.Windows.Data;
 
 namespace FMMEditor.ViewModels
 {
-    /// <summary>
-    /// Display model for People with resolved names
-    /// </summary>
-    public class PeopleDisplayModel
-    {
-        public People Person { get; set; }
-        public int Uid => Person.Uid;
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-        public string CommonName { get; set; } = "";
-        public string FullName => !string.IsNullOrEmpty(CommonName) ? CommonName : $"{FirstName} {LastName}".Trim();
-        public string NationName { get; set; } = "";
-        public string ClubName { get; set; } = "";
-        public string DateOfBirth => DateOfBirthConverter.ToDateString(Person.DateOfBirth);
-        public string PersonType => Person.Type switch { 0 => "Player", 1 => "Staff", _ => "Other" };
-        public short NationalCaps => Person.NationalCaps;
-        public short NationalGoals => Person.NationalGoals;
-        public byte Adaptability => Person.Adaptability;
-        public byte Ambition => Person.Ambition;
-        public byte Loyalty => Person.Loyality;
-        public byte Pressure => Person.Pressure;
-        public byte Professionalism => Person.Professionalism;
-        public byte Temperament => Person.Temperament;
-
-        public PeopleDisplayModel(People person)
-        {
-            Person = person;
-        }
-    }
-
     public class PeopleViewModel : ReactiveObject
     {
         public extern string? FolderPath { [ObservableAsProperty] get; }
