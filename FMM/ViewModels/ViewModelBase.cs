16a48a209fd68218e90138f1439b58c4d061233d
Ibnu Hasan A. <ibnu.haar@gmail.com>
2025-12-26 10:38:36 +0700

    Initial project setup with Avalonia cross-platform apps
    Add solution structure for FMMEditor with Avalonia-based projects targeting Android, iOS, Desktop, and Browser. Includes shared core project, platform-specific project files, resources, and central package management. Also updates .gitignore for platform and IDE specifics.

---
+++ FMM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FMM.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ FMM/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FMM.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
